75bfe857393a6f7d64174ca74df0c05790f9a074
abdroos <abd.alaidrous@gmail.com>
2024-08-13 15:40:10 +0300

    refactor: add nuxt link

---
+++ components/Common/Footer.vue
@@ -0,0 +1,136 @@
+<template>
+  <!-- Start Footer Section -->
+  <footer>
+    <div class="footer-container">
+      <div class="container pb-80 pt-80 ontop">
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="logo">
+              <img src="/assets/imgs/logo-light.png" alt="" />
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="column">
+              <h6 class="sub-title mb-30">{{ $t('components.common.footer.section.socialMedia.title') }}</h6>
+              <ul class="m-0 p-0">
+                <li class="hover-this cursor-pointer mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://github.com/6degrees" class="hover-anim">{{ $t('components.common.footer.section.socialMedia.links.github') }}</a>
+                </li>
+                <li class="hover-this cursor-pointer mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://github.com/6degrees" class="hover-anim">{{ $t('components.common.footer.section.socialMedia.links.behance') }}</a>
+                </li>
+                <li class="hover-this cursor-pointer mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://twitter.com/6degrees_sa" class="hover-anim">{{ $t('components.common.footer.section.socialMedia.links.twitter') }}</a>
+                </li>
+                <li class="hover-this cursor-pointer mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://www.linkedin.com/company/6degz" class="hover-anim">{{ $t('components.common.footer.section.socialMedia.links.linkedin') }}</a>
+                </li>
+                <li class="hover-this cursor-pointer mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://instagram.com/6degrees_sa" class="hover-anim">{{ $t('components.common.footer.section.socialMedia.links.instagram') }}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <!-- Uncomment and use if needed
+          <div class="col-lg-3">
+              <div class="column">
+                  <h6 class="sub-title mb-30">{{ $t('components.common.footer.section.office.title') }}</h6>
+                  <p>{{ $t('components.common.footer.section.office.address') }}</p>
+                  <h5 class="mt-15 underline">
+                      <a href="#0">{{ $t('components.common.footer.section.office.phone') }}</a>
+                  </h5>
+              </div>
+          </div>
+          -->
+        </div>
+      </div>
+      <div class="container bord pt-30 pb-30 bord-thin-top">
+        <div class="row">
+          <div class="col-lg-6">
+            <div class="links">
+              <ul class="m-0 p-0">
+                <li class="mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://up.monitoring.6d.com.sa/status/main" class="animsition-link">{{ $t('components.common.footer.section.links.serviceStatus') }}</a>
+                </li>
+                <li class="mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="/about" class="animsition-link">{{ $t('components.common.footer.section.links.terms') }}</a>
+                </li>
+                <li class="mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="/contact" class="animsition-link">{{ $t('components.common.footer.section.links.privacy') }}</a>
+                </li>
+                <li class="mr-9 rtl:mr-0 rtl:ml-9">
+                  <a href="https://wiki.6degrees.com.sa/" class="animsition-link">{{ $t('components.common.footer.section.links.docs') }}</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-lg-6">
+            <div class="copyright d-flex">
+              <div class="ltr:ml-auto rtl:mr-auto">
+                <p class="fz-13">
+                  {{ $t('components.common.footer.section.copyright.text') }}
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- End Footer Section -->
+</template>
+
+<script setup>
+/*
+|--------------------------------------------------------------------------
+| handleResize Function
+|--------------------------------------------------------------------------
+|
+| Sets up GSAP animation and ScrollTrigger based on viewport size.
+| The animation will move the footer container from 50% to 0% vertical position when scrolled.
+| This function is called initially and on window resize.
+|
+*/
+const handleResize = () => {
+  if (window.innerWidth > 991) {
+    gsap.set(".footer-container", { yPercent: -50 }); // Set initial vertical position of the footer container
+    const uncover = gsap.timeline({ paused: true }); // Create a new GSAP timeline, initially paused
+    uncover.to(".footer-container", { yPercent: 0, ease: "none" }); // Define animation to move footer container to final position
+
+    ScrollTrigger.create({
+      trigger: "main", // Element that triggers the animation
+      start: "bottom bottom", // Animation start point (bottom of main element reaches bottom of viewport)
+      end: "+=50%", // Animation end point (50% of viewport height from start point)
+      animation: uncover, // Animation to be triggered
+      scrub: true, // Smoothly animate according to scroll position
+    });
+  }
+};
+
+/*
+|--------------------------------------------------------------------------
+| onMounted Lifecycle Hook
+|--------------------------------------------------------------------------
+|
+| Called when the component is mounted. Initializes the resize handling and
+| adds an event listener for window resize to adjust animations accordingly.
+|
+*/
+onMounted(() => {
+  handleResize(); // Initialize animation and check viewport size on mount
+  window.addEventListener("resize", handleResize); // Add resize event listener
+});
+
+/*
+|--------------------------------------------------------------------------
+| onBeforeUnmount Lifecycle Hook
+|--------------------------------------------------------------------------
+|
+| Called before the component is unmounted. Removes the resize event listener
+| to prevent memory leaks and unwanted behavior.
+|
+*/
+onBeforeUnmount(() => {
+  window.removeEventListener("resize", handleResize); // Cleanup event listener on unmount
+});
+</script>
---
+++ locales/en/index.js
@@ -0,0 +1,183 @@
+export default {
+    'components': {
+        'common': {
+            'navbar': {
+                'menu': {
+                    'title': 'Menu'
+                },
+            },
+            'footer': {
+                'section': {
+                    'socialMedia': {
+                        'title': 'Social Media',
+                        'links': {
+                            'github': 'GitHub',
+                            'behance': 'Behance',
+                            'twitter': 'X',
+                            'linkedin': 'LinkedIn',
+                            'instagram': 'Instagram'
+                        }
+                    },
+                    'office': {
+                        'title': 'Our Office:',
+                        'address': '6919 Prince Turki Road, Al Khobar Corniche, Office #301, Al Khobar 34413',
+                        'phone': '0558412255'
+                    },
+                    'links': {
+                        'serviceStatus': 'Service Status',
+                        'terms': 'Terms',
+                        'privacy': 'Privacy',
+                        'docs': 'Docs'
+                    },
+                    'copyright': {
+                        'text': 'All rights reserved © 6 Degrees Technologies Est 2024.'
+                    }
+                }
+            },
+            'menu': {
+                'section': {
+                    'menu': {
+                        'home': 'Home',
+                        'about': 'About',
+                        'contact': 'Contact'
+                    },
+                    'contactInfo': {
+                        'headQuarter': {
+                            'title': 'Head Quarter',
+                            'address': '6919 Prince Turki Road, Al Khobar Corniche, Office #301, Al Khobar 34413'
+                        },
+                        'howToConnect': {
+                            'title': 'How to Connect',
+                            'phone': '+966 55 841 2255',
+                        },
+                        'joinUs': {
+                            'title': 'Join Us',
+                            'socialMedia': {
+                                'github': 'GitHub',
+                                'behance': 'Behance',
+                                'facebook': 'Facebook',
+                                'twitter': 'X',
+                                'linkedin': 'LinkedIn',
+                                'instagram': 'Instagram'
+                            }
+                        }
+                    }
+                }
+            }
+        },
+        'landing': {
+            'header': {
+                'title': 'Connecting Futures',
+                'subtitle': 'Innovating Saudi Arabia’s Digital Landscape',
+            },
+            'about': {
+                'section': {
+                    'subtitle': '- About Us',
+                    'intro': {
+                        'line1': 'An IT business enabler and partner',
+                        'line2': 'We aim to be the link between you',
+                        'line3': 'and your technological goal'
+                    },
+                    'services': {
+                        'design': {
+                            'number': '01',
+                            'title': 'Design',
+                            'subtitle': 'UI/UX',
+                            'link': 'View Details'
+                        },
+                        'development': {
+                            'number': '02',
+                            'title': 'Development',
+                            'subtitle': 'Website Development',
+                            'link': 'View Details'
+                        },
+                        'iot': {
+                            'number': '03',
+                            'title': 'IoT',
+                            'subtitle': 'Smart Building Automation',
+                            'link': 'View Details'
+                        }
+                    }
+                }
+            },
+            'features': {
+                'section': {
+                    'title': 'Why 6 DEGREES',
+                    'allFeatures': 'All Features',
+                }
+            },
+            'technologies': {
+                'section': {
+                    'title': 'Our Technology Stack',
+                    'subtitle': 'The Best in the Industry',
+                }
+            },
+            'blogs': {
+                'section': {
+                    'title': 'Our Featured',
+                    'subtitle': 'Highlights',
+                }
+            },
+            'contacts': {
+                'section': {
+                    'title': '- Contact Us',
+                    'heading': 'Get In <span class="f-ultra-light">Touch</span>.',
+                    'contactForm': {
+                        'title': 'Contact Form'
+                    },
+                    'phone': {
+                        'title': 'Phone Number(s)',
+                        'numbers': {
+                            'primary': '+966 55 841 2255',
+                            'secondary': '+966 53 786 2922',
+                            'note': '(For SMS only)'
+                        }
+                    },
+                    'email': {
+                        'title': 'Email Address',
+                    },
+                    'address': {
+                        'title': 'Address',
+                        'details': '6919 Prince Turki Road, Corniche,<br>Khobar, Eastern Region 34413,<br>Saudi Arabia',
+                        'map': 'Google Maps Location'
+                    }
+                }
+            },
+        },
+        'about': {
+            'header': {
+                'section': {
+                    'title': 'About Us',
+                    'subtitle': '6 Degrees Technologies',
+                    'image': {
+                        'altText': 'About Us Background Image'
+                    }
+                }
+            },
+            "intro": {
+                "section": {
+                    "whoWeAre": {
+                        "subtitle": "Who We Are?",
+                        "description": "We are a pioneering tech company based in Saudi Arabia, dedicated to driving digital transformation and innovation across various sectors. Our journey began with a vision to bridge the gap between technology and business needs, and today, we are proud to be a trusted partner for numerous businesses in the Kingdom.",
+                        "text": "An IT business enabler and partner"
+                    },
+                    "services": {
+                        "uiUx": "UI/UX Design",
+                        "websiteDevelopment": "Website Development",
+                        "applicationDevelopment": "Application Development",
+                        "seo": "SEO",
+                        "hardwareImport": "Hardware Import",
+                        "smartBuildingAutomation": "Smart Building Automation",
+                        "apiIntegration": "API Integration",
+                        "dataMigration": "Data Migration"
+                    },
+                    "numbers": {
+                        "yearsOfExperience": "+25 Years of Experience",
+                        "completedProjects": "+50 Completed Projects",
+                        "satisfiedClients": "+200 Satisfied Clients"
+                    }
+                }
+            }
+        }
+    },
+};
