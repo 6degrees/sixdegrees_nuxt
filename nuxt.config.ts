8575367e3dc27a78dc184765297d63e2cd28e93d
abdroos <abd.alaidrous@gmail.com>
2024-08-18 11:28:17 +0300

    refactor: add wss in vite

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -75,6 +75,13 @@ export default defineNuxtConfig({
     '@nuxtjs/i18n',
   ],
   vite: {
+    server: {
+      hmr: {
+        protocol: 'wss',
+        clientPort: 443,
+        path: 'hmr/'
+      }
+    },
     plugins: [
       react()
     ]
