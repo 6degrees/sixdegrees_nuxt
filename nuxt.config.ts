ee5ea01f38cc907970524e2703e912c4cd70e1de
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2024-09-03 15:55:30 +0300

    uninstall bootstrap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   ssr: true,
-  devtools: {enabled: false},
+  devtools: { enabled: false },
   typescript: {
     shim: false
   },
@@ -18,42 +18,36 @@ export default defineNuxtConfig({
         lang: 'en'
       },
       meta: [
-        {name: "viewport", content: "width=device-width, initial-scale=1"},
-        {charset: "utf-8"},
-        {"http-equiv": 'X-UA-Compatible', content: "IE=edge"},
-        {name: 'keywords', content: ''},
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { charset: "utf-8" },
+        { "http-equiv": 'X-UA-Compatible', content: "IE=edge" },
+        { name: 'keywords', content: '' },
         {
           name: 'description',
           content: 'Where your technological future, comes today. The limits to technology uses is beyond imagination, digitize your life with us'
         },
-        {name: 'author', content: 'Six Degrees Technologies'}
+        { name: 'author', content: 'Six Degrees Technologies' }
       ],
       link: [
-        {rel: 'shortcut icon', href: '/assets/imgs/favicon.ico'},
+        { rel: 'shortcut icon', href: '/assets/imgs/favicon.ico' },
         // Google Fonts
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap'
         },
-        // CSS
-        {rel: 'stylesheet', href: '/assets/fonts/mona-sans/style.css'},
-        {rel: 'stylesheet', href: '/assets/css/plugins.css'},
-        {rel: 'stylesheet', href: '/assets/css/style.css'},
-        {
-          rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.5.0/font/bootstrap-icons.min.css'
-        },
+        { rel: 'stylesheet', href: '/assets/fonts/mona-sans/style.css' },
+        { rel: 'stylesheet', href: '/assets/css/plugins.css' },
+        { rel: 'stylesheet', href: '/assets/css/style.css' },
       ],
       script: [
-        {src: '/assets/js/bootstrap.bundle.min.js'},
-        {src: '/assets/js/plugins.js'},
-        {src: '/assets/js/isotope.pkgd.min.js'},
-        {src: '/assets/js/wow.min.js'},
-        {src: '/assets/js/gsap.min.js'},
-        {src: '/assets/js/ScrollTrigger.min.js'},
-        {src: '/assets/js/ScrollSmoother.min.js'},
-        {src: '/assets/js/scripts.js', defer: true},
-        {src: 'https://unpkg.com/@splinetool/viewer@1.9.3/build/spline-viewer.js', type: 'module'} // Add Spline viewer script
+        { src: '/assets/js/plugins.js' },
+        { src: '/assets/js/isotope.pkgd.min.js' },
+        { src: '/assets/js/wow.min.js' },
+        { src: '/assets/js/gsap.min.js' },
+        { src: '/assets/js/ScrollTrigger.min.js' },
+        { src: '/assets/js/ScrollSmoother.min.js' },
+        { src: '/assets/js/scripts.js', defer: true },
+        { src: 'https://unpkg.com/@splinetool/viewer@1.9.3/build/spline-viewer.js', type: 'module' } // Add Spline viewer script
       ]
     }
   },
