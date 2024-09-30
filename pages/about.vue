9fd75dfbce94d77176b7d8ef83e2ff63fd11df70
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2024-09-30 11:54:54 +0300

    -

--- pages/about.vue
+++ pages/about.vue
@@ -58,7 +58,7 @@ useHead({
     // Open Graph Tags
     { property: 'og:title', content: '6 Degrees Technologies - About Us' },
     { property: 'og:description', content: 'An IT business enabler and partner, we aim to be the link between you and your technological goal' },
-    { property: 'og:image', content: '/assets/imgs/meta/5.png' },
+    { property: 'og:image', content: '/assets/imgs/meta/6.png' },
     { property: 'og:url', content: process.client ? window.location.href : '' },
     { property: 'og:type', content: 'website' },
 
@@ -66,7 +66,7 @@ useHead({
     { name: 'twitter:card', content: 'summary_large_image' },
     { name: 'twitter:title', content: '6 Degrees Technologies - About Us' },
     { name: 'twitter:description', content: 'An IT business enabler and partner, we aim to be the link between you and your technological goal' },
-    { name: 'twitter:image', content: '/assets/imgs/meta/5.png' },
+    { name: 'twitter:image', content: '/assets/imgs/meta/6.png' },
     { name: 'twitter:url', content: process.client ? window.location.href : '' } ,
   ],
   bodyAttrs: {
--- pages/index.vue
+++ pages/index.vue
@@ -67,7 +67,7 @@ useHead({
     { name: 'description', content: 'An IT business enabler and partner, we aim to be the link between you and your technological goal' },
     
     // Open Graph Tags
-    { property: 'og:title', content: '6 Degrees Technologies - About Us' },
+    { property: 'og:title', content: '6 Degrees Technologies' },
     { property: 'og:description', content: 'An IT business enabler and partner, we aim to be the link between you and your technological goal' },
     { property: 'og:image', content: '/assets/imgs/meta/5.png' },
     { property: 'og:url', content: process.client ? window.location.href : '' },
@@ -75,7 +75,7 @@ useHead({
 
     // Twitter Card Tags
     { name: 'twitter:card', content: 'summary_large_image' },
-    { name: 'twitter:title', content: '6 Degrees Technologies - About Us' },
+    { name: 'twitter:title', content: '6 Degrees Technologies' },
     { name: 'twitter:description', content: 'An IT business enabler and partner, we aim to be the link between you and your technological goal' },
     { name: 'twitter:image', content: '/assets/imgs/meta/5.png' },
     { name: 'twitter:url', content: process.client ? window.location.href : '' } ,
