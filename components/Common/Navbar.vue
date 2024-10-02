ee6ecdb57fa36277623cecd05b373f36f15c7db0
Atheer F. Alotaibi <84769445+atheer0998@users.noreply.github.com>
2024-10-02 11:11:35 +0300

    fix the navbar issue

--- components/Common/Navbar.vue
+++ components/Common/Navbar.vue
@@ -5,21 +5,20 @@
       <!-- Logo Section -->
       <div :class="`logo w-34 h-auto lg:w-32 icon-img-${borderBottom ? '100' : '90'}`">
         <NuxtLink :to="pageRout.toHomePage()">
-          <!-- Increased logo size with Tailwind width and height classes -->
           <img src="/assets/imgs/logo-light.png" alt="6 Degrees Technologies" loading="lazy" class="w-34 h-auto lg:w-32" />
         </NuxtLink>
       </div>
 
       <!-- Language Switcher with border as a button -->
       <div class="ml-auto rtl:mr-auto rtl:ml-0 flex items-center space-x-2 rtl:space-x-reverse">
-        <a :href="availableLocales[0].hrf" class="animsition-link language-switcher-button" @click="toggleMenu">
+        <a :href="availableLocales[0].hrf" class="animsition-link language-switcher-button">
           <span class="text-md lg:text-md">{{ availableLocales[0].name }}</span>
         </a>
       </div>
 
       <!-- Menu Icon -->
       <div class="menu-icon cursor-pointer ml-4 rtl:mr-4 rtl:ml-0" @click="toggleMenu" aria-label="Toggle navigation menu">
-        <span class="icon text-2xl lg:text-3xl"> <!-- Increased icon size -->
+        <span class="icon text-2xl lg:text-3xl">
           <i></i>
           <i></i>
         </span>
